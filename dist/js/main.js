fd7ceee97286c7c631371f862aa7446052696ee5
n3kcr0n <ronronmacayaon@gmail.com>
2019-07-11 20:08:33 +0800

    Initial Commit

---
+++ dist/js/main.js
@@ -0,0 +1,18 @@
+$('#year').text(new Date().getFullYear());
+
+$('body').scrollspy({
+    target: '#main-nav'
+});
+
+$('#main-nav a').on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 120
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
+});
